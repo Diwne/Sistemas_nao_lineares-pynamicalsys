1c8805c24d178db701e6477a83b7edc6a12ebdb7
Zero <rdias6077@gmail.com>
2025-11-28 20:35:48 -0300

    Add files via upload

---
+++ AtratorLorenz.py
@@ -0,0 +1,37 @@
+# -- mudar nome do projeto --
+import pynamicalsys, numpy as pd, matplotlib.pyplot as plt, seaborn as sns
+from pynamicalsys import ContinuousDynamicalSystem as cds
+from pynamicalsys import PlotStyler
+
+ds = cds(model = "lorenz system")
+ds.intergrator("rk4", time_sleep = 0.005)
+
+
+
+
+parameters = [10, 28, 8/3]    # -- parametros caoticos classicos --
+u = [0.1, 0.1, 0.1]    # -- condiçoes iniciais --
+tempo_total = 100    # -- tempo total de evoluçao --
+trajectory = ds.trajectory(u, tempo_total, parameters = parameters)
+
+print(trajectory.shape)
+
+ps = PlotStyler(linewidth = 0.5)
+ps.apply_style()
+
+plt.plot(trajectory[:, 1], trajectory[:, 3], "k-")
+
+plt.xlabel("$x$")
+plt.ylabel("$y$")
+plt.show
+
+ps = PlotStyler(fontsize=18, linewidth=0.3)  # Style for attractor plot
+ps.apply_style()
+
+# Plot the Lorenz attractor projection on the x-z plane
+plt.plot(trajectory[:, 1], trajectory[:, 3], "k-")
+
+plt.xlabel("$x$")
+plt.ylabel("$z$")
+
+plt.show()  # Display the attractor plot
